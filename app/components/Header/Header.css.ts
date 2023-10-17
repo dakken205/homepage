f0c1e820aaf6b40c33db322f82f5763c8edb4c5d
dike-okayama <lawliet.d.k.m@gmail.com>
2023-10-18 04:05:01 +0900

    Make Header sticky

--- app/components/Header/Header.css.ts
+++ app/components/Header/Header.css.ts
@@ -1,7 +1,7 @@
 import { style } from "@vanilla-extract/css";
 
 export const header = style({
-  position: "fixed",
+  position: "sticky",
   top: 0,
   display: "flex",
   flexDirection: "column",
