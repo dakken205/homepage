8befc7f49a3f05e1d30d63248418e9f65bab738b
dike-okayama <lawliet.d.k.m@gmail.com>
2023-10-18 04:05:26 +0900

    Implement Footer

--- app/components/Footer/Footer.css.ts
+++ app/components/Footer/Footer.css.ts
@@ -1,6 +1,48 @@
 import { style } from "@vanilla-extract/css";
 
-export const footer = style({});
+export const footer = style({
+  padding: "36px 0",
+  width: "100%",
+  minHeight: "400px",
+});
 
-export const footerGrid = style({});
-export const stack = style({});
+export const wrapper = style({
+  width: "1248px",
+  margin: "0 auto",
+});
+
+export const footerGrid = style({
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "space-between",
+});
+
+export const stack = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "stretch",
+  justifyContent: "flex-start",
+  flex: "inherit",
+  gap: "16px",
+});
+
+export const copyright = style({
+  lineHeight: "20px",
+  fontSize: "14px",
+  color: "#888",
+});
+
+export const socials = style({
+  display: "flex",
+  gap: "12px",
+});
+
+export const socialIcon = style({
+  color: "#888",
+  transition: "color 0.15s ease",
+  selectors: {
+    "&:hover": {
+      color: "#fff",
+    },
+  },
+});
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
