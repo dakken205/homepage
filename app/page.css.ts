aaa4104c88a84af0c373a83bcf58261ea651b6f0
dike-okayama <lawliet.d.k.m@gmail.com>
2023-10-18 17:25:58 +0900

    Implement Home

--- app/page.css.ts
+++ app/page.css.ts
@@ -1,3 +1,81 @@
-import { style } from "@vanilla-extract/css";
+import { style, globalStyle } from "@vanilla-extract/css";
 
-export const home = style({});
+export const homeRoot = style({});
+
+export const introRoot = style({
+  width: "100%",
+  maxWidth: "calc(1234px + 56px * 2)",
+  textAlign: "center",
+  margin: "80px auto",
+  padding: "0 56px",
+  overflow: "hidden",
+  isolation: "isolate",
+});
+
+export const introTitle = style({
+  fontWeight: "800",
+  fontSize: "max(48px, min(5vw, 76px))",
+  lineHeight: "1",
+  letterSpacing: "-.0.5em",
+  padding: "24px",
+});
+
+export const introSubtitle = style({
+  color: "#888",
+  fontSize: "max(15px, min(2vw, 20px))",
+  lineHeight: "1.8",
+  letterSpacing: "-0.01em",
+  fontWeight: "400",
+  padding: "40px 32px",
+  width: "calc(100% - 1% * 2)",
+  left: "1%",
+});
+
+globalStyle(`${introSubtitle} strong`, {
+  fontWeight: "500",
+  color: "#fafafa",
+});
+
+export const introButtons = style({
+  height: "64px",
+});
+
+export const introButtonBase = style({
+  display: "inline-flex",
+  height: "48px",
+  fontSize: "1rem",
+  lineHeight: "1.5rem",
+  borderRadius: "8px",
+  fontWeight: "500",
+  padding: "0 12px",
+  margin: "0 8px",
+  boxShadow: "0 0 0 1px transparent",
+  maxWidth: "100%",
+  alignItems: "center",
+  transitionProperty: "border-color, background, color, transform, box-shadow",
+  transitionDuration: ".15s",
+  transitionTimingFunction: "ease",
+  cursor: "pointer",
+  userSelect: "none",
+});
+
+export const introWhiteButton = style({
+  backgroundColor: "#fff",
+  color: "#000",
+  selectors: {
+    "&:hover": {
+      backgroundColor: "#ededed",
+    },
+  },
+});
+
+export const introBlackButton = style({
+  backgroundColor: "#000",
+  color: "#fff",
+  border: "1px solid hsla(0,0%, 100%, .14)",
+  selectors: {
+    "&:hover": {
+      backgroundColor: "#0a0a0a",
+    },
+  },
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,33 @@
+import Link from "next/link";
 import * as styles from "./page.css";
 
 export default function Home() {
-  return <div className={styles.home}></div>;
+  return (
+    <div className={styles.homeRoot}>
+      <div className={styles.introRoot}>
+        <h1 className={styles.introTitle}>DA研HPへようこそ!</h1>
+        <div className={styles.introSubtitle}>
+          <p>データ分析研究会(通称DA研)は兵庫県立大学の学生による部活です。</p>
+          <p>
+            「<strong>データサイエンスのセミプロ集団へ</strong>
+            」を合言葉に、勉強会の開催やコンペティションへの参加等を通じて、日々技術力を高めています。
+          </p>
+        </div>
+        <div className={styles.introButtons}>
+          <Link
+            className={`${styles.introButtonBase} ${styles.introWhiteButton}`}
+            href="/activities"
+          >
+            DA研を知る
+          </Link>
+          <Link
+            className={`${styles.introButtonBase} ${styles.introBlackButton}`}
+            href="/contact"
+          >
+            DA研に入る
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
