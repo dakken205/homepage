af59b7301b7fe933a868566f881fdc249b801e53
dike-okayama <lawliet.d.k.m@gmail.com>
2023-10-27 04:32:56 +0900

    Implement features and promotion to top-page

--- app/page.css.ts
+++ app/page.css.ts
@@ -42,18 +42,19 @@ globalStyle(`${introSubtitle} strong`, {
 export const introButtons = style({
   display: "flex",
   justifyContent: "center",
-  height: "48px",
+  height: "96px",
 });
 
 export const introButtonBase = style({
   display: "flex",
   fontSize: "1rem",
   width: "8rem",
+  height: "48px",
   lineHeight: "1.5rem",
   borderRadius: "8px",
   fontWeight: "500",
   padding: "0 12px",
-  margin: "0 8px",
+  margin: "auto 8px",
   boxShadow: "0 0 0 1px transparent",
   maxWidth: "100%",
   alignItems: "center",
@@ -86,10 +87,25 @@ export const introBlackButton = style({
   },
 });
 
-export const departmentsRoot = style({
+export const visionRoot = style({
   maxWidth: "1200px",
   margin: "0 auto",
   padding: "0 16px 96px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  textAlign: "center",
+});
+
+export const departmentsRoot = style({
+  background: "linear-gradient(180deg, #000 30%, #111 50%, #111 100%)",
+  borderBottom: "1px solid #333",
+  padding: "0 16px 96px",
+});
+
+export const department = style({
+  maxWidth: "1200px",
+  margin: "0 auto",
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
@@ -123,27 +139,107 @@ export const departmentsGrid = style({
   marginTop: "48px",
 });
 
-export const featureCard = style({
+export const activityCard = style({
   minHeight: "380px",
   display: "flex",
   flexDirection: "column",
   justifyContent: "flex-end",
-  padding: "12px",
+  padding: "24px",
   borderRadius: "12px",
   border: "1px solid #333",
   transitionProperty: "border-color, background",
   transitionDuration: ".15s",
 });
 
-export const featureCardTitle = style({
+export const activityCardMini = style({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  padding: "24px",
+  borderRadius: "12px",
+  border: "1px solid #333",
+  transitionProperty: "border-color, background",
+  transitionDuration: ".15s",
+});
+
+export const activityCardTitle = style({
   fontSize: "20px",
   fontWeight: "600",
   lineHeight: "32px",
   color: "#fff",
 });
 
-export const featureCardSubtitle = style({
+export const activityCardSubtitle = style({
   fontSize: "14px",
   lineHeight: "1.5",
   color: "#888",
+});
+
+export const featuresRoot = style({
+  maxWidth: "1200px",
+  margin: "0 auto",
+  padding: "96px 16px",
+});
+
+export const featuresMain = style({});
+
+export const featuresTitle = style({
+  fontSize: "32px",
+});
+
+export const features = style({
+  margin: "48px 0",
+  color: "#888",
+});
+
+globalStyle(`${features} li`, {
+  margin: "5px 0",
+});
+
+export const promotionRoot = style({
+  maxWidth: "1500px",
+  display: "flex",
+  flexDirection: "column",
+  margin: "0 auto",
+  padding: "60px 16px 96px",
+  overflow: "hidden",
+});
+
+export const promotionTitle = style({
+  width: "100%",
+  marginBottom: "48px",
+  textAlign: "center",
+  fontSize: "32px",
+  lineHeight: "1.25",
+  fontWeight: "700",
+  color: "#fafafa",
+});
+
+export const promotions = style({
+  display: "flex",
+  flexDirection: "row",
+  gap: "16px",
+  position: "relative",
+  selectors: {
+    "&::before": {
+      content: "",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      background: "linear-gradient(transparent 0%, #000 120%)",
+      zIndex: "2",
+    },
+  },
+});
+
+export const promotionsStack = style({
+  position: "relative",
+  display: "flex",
+  flexDirection: "column",
+  gap: "16px",
+});
+
+export const promotionCard = style({
+  borderRadius: "6px",
+  overflow: "hidden",
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import * as styles from "./page.css";
 import {
   BusinessAnimation,
@@ -35,56 +36,151 @@ export default function Home() {
           </Link>
         </div>
       </main>
+
       <section className={styles.departmentsRoot}>
-        <h2 className={`${styles.departmentsTitle} ${styles.textGradient}`}>
-          DA研の部門構成
-        </h2>
-        <p className={styles.departmentsSubtitle}>
-          弊部では、一般社団法人データサイエンティスト協会が定める
-          <Link
-            href="https://www.datascientist.or.jp/dssjournal/2021/07/16/dodv17/"
-            target="_blank"
-          >
-            「データサイエンティストに求められるスキルセット」
-          </Link>
-          に基づき、以下の3つの部門に分かれて活動しています。
-        </p>
-        <div className={styles.departmentsGrid}>
-          <div>
-            <Link href="/department/biz" className={styles.featureCard}>
+        <div className={styles.department}>
+          <h2 className={`${styles.departmentsTitle} ${styles.textGradient}`}>
+            DA研の部門活動
+          </h2>
+          <p className={styles.departmentsSubtitle}>
+            弊部では、一般社団法人データサイエンティスト協会が定める
+            <Link
+              href="https://www.datascientist.or.jp/dssjournal/2021/07/16/dodv17/"
+              target="_blank"
+            >
+              「データサイエンティストに求められるスキルセット」
+            </Link>
+            に基づき、以下の3つの部門に分かれて活動しています。
+          </p>
+          <div className={styles.departmentsGrid}>
+            <Link href="/department/biz" className={styles.activityCard}>
               <BusinessAnimation />
-              <div className={styles.featureCardTitle}>
+              <div className={styles.activityCardTitle}>
                 Biz部門 / Business Problem Solving
               </div>
-              <div className={styles.featureCardSubtitle}>
+              <div className={styles.activityCardSubtitle}>
                 課題背景を理解した上で、ビジネス課題を整理し、解決する。
               </div>
             </Link>
-          </div>
-          <div>
-            <Link href="/departments/ds" className={styles.featureCard}>
+            <Link href="/departments/ds" className={styles.activityCard}>
               <DataScienceAnimation />
-              <div className={styles.featureCardTitle}>
+              <div className={styles.activityCardTitle}>
                 DS部門 / Data Science
               </div>
-              <div className={styles.featureCardSubtitle}>
+              <div className={styles.activityCardSubtitle}>
                 情報処理、人工知能、統計学などの情報科学系の知恵を理解し、データ分析に応用する。
               </div>
             </Link>
-          </div>
-          <div>
-            <Link href="/department/de" className={styles.featureCard}>
+            <Link href="/department/de" className={styles.activityCard}>
               <DataEngineeringAnimation />
-              <div className={styles.featureCardTitle}>
+              <div className={styles.activityCardTitle}>
                 DE部門 / Data Engineering
               </div>
-              <div className={styles.featureCardSubtitle}>
+              <div className={styles.activityCardSubtitle}>
                 データサイエンスを意味のある形に使えるようにし、実装、運用する。
               </div>
             </Link>
+            <ActivityCardMini
+              href="/department/biz#cambria"
+              title="カンブリア宮殿の同時視聴"
+              subTitle="日経スペシャル カンブリア宮殿を毎週視聴します。視聴の後は、ビジネスの視点からディスカッションを行います。"
+            />
+            <ActivityCardMini
+              href="/department/ds#machine-coder"
+              title="Machine Coder"
+              subTitle="隔週で開催されるテーブル形式の部内コンペです。データから「難破船から生存するか」「糖尿病を発症するか」などを予測し、その精度を競います。"
+            />
+            <ActivityCardMini
+              href="/department/de#algorithm"
+              title="AtCoder / アルゴリズム勉強会"
+              subTitle="AtCoderをはじめ、数多くのプログラミングコンテストに参加します。論理思考や問題解決能力を養う上で非常に有益であると考えます。"
+            />
+            <ActivityCardMini
+              href="/department/biz#promotion"
+              title="販促コンペ"
+              subTitle=""
+            />
+            <ActivityCardMini
+              href="/department/ds#kaggle"
+              title="Kaggleコンペティション"
+              subTitle="Kaggleは世界最大級のデータ分析のコンペティションサイトです。"
+            />
+            <ActivityCardMini
+              href="/department/de#develop"
+              title="Webアプリ開発"
+              subTitle="多くの人に感動を与えるアプリを作りましょう。"
+            />
           </div>
         </div>
       </section>
+
+      <section className={styles.featuresRoot}>
+        <div className={styles.featuresMain}>
+          <h2 className={`${styles.featuresTitle} ${styles.textGradient}`}>
+            DA研はこんな方におすすめです
+          </h2>
+          <div className={styles.features}>
+            <ul>
+              <li>
+                ✓ 講義を受講しているだけでデータサイエンティストになれるか不安
+              </li>
+              <li>✓ 情報共有／切磋琢磨できる仲間がほしい</li>
+              <li>
+                ✓ 実務や団体コンペ等を通して、より実践的な学びの機会を得たい
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section className={styles.promotionRoot}>
+        <h3 className={`${styles.promotionTitle} ${styles.textGradient}`}>
+          部門の枠を超えた活動
+        </h3>
+        <div className={styles.promotions}>
+          <div className={styles.promotionsStack}>
+            <Link
+              href="https://twitter.com/dakken205/status/1716673143782396258"
+              target="_blank"
+              className={styles.promotionCard}
+            >
+              <Image
+                width={350}
+                height={270}
+                src="/promotions/01.jpeg"
+                alt="商大歳2023 DA研ポスター"
+              ></Image>
+            </Link>
+            <Link
+              href="https://twitter.com/dakken205/status/1694397857271628051"
+              target="_blank"
+              className={styles.promotionCard}
+            >
+              <Image
+                width={350}
+                height={400}
+                src="/promotions/02.jpeg"
+                alt="DA研2023 夏合宿 1日目"
+              ></Image>
+            </Link>
+          </div>
+          <div className={styles.promotionsStack}></div>
+        </div>
+      </section>
     </div>
+  );
+}
+
+type ActivityCardProps = {
+  href: string;
+  title: string;
+  subTitle: string;
+};
+
+function ActivityCardMini({ href, title, subTitle }: ActivityCardProps) {
+  return (
+    <Link className={styles.activityCardMini} href={href}>
+      <div className={styles.activityCardTitle}>{title}</div>
+      <div className={styles.activityCardSubtitle}>{subTitle}</div>
+    </Link>
   );
 }
