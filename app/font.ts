f3cad00085d678dbc7a820d76209db150d629874
dike-okayama <lawliet.d.k.m@gmail.com>
2023-10-28 18:35:24 +0900

    Unify font loading method

--- app/font.ts
+++ app/font.ts
@@ -1,4 +1,6 @@
-import { Zen_Kurenaido } from "next/font/google";
+import { Inter, Zen_Kurenaido } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
 
 export const zen_kurenaido = Zen_Kurenaido({
   weight: "400",
